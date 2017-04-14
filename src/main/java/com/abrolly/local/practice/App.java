899ba66ff46c22591f36cbfbc59c8de2c8136fb3
saroj <sarojkmb@gmail.com>
2017-04-14 12:29:30 -0400

    second commit

---
+++ src/main/java/com/abrolly/local/practice/App.java
@@ -0,0 +1,19 @@
+package com.abrolly.local.practice;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+    	
+        System.out.println("Hello!");
+        
+       
+    }
+}
+
